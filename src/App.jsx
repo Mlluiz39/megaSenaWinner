2eb948e61d636291faf0269bf542208172163a9d
mlluiz <mlluizpereira39@gmail.com>
2024-03-17 13:24:37 -0300

    add number sort

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import './App.css'
 import megaLogo from './assets/logo-mega.png'
 
 const MegaSenaSorteio = () => {
-  const numerosMaisSorteados = [
-    5, 10, 21, 22, 36, 54, 4, 16, 26, 49, 51, 54, 9, 18, 26, 27, 35, 47, 17, 38, 44, 53, 23, 33, 34, 37, 41, 16,26, 49, 51, 54, 30,32, 34, 37, 41, 16, 26, 49, 51, 54
-  ]
   const [numerosSorteados, setNumerosSorteados] = useState([])
 
   const sortearNumeros = () => {
+    const numerosMaisSorteados = [
+      5, 10, 21, 22, 36, 54, 4, 16, 26, 49, 51, 54, 9, 18, 26, 27, 35, 47, 17,
+      38, 44, 53, 23, 33, 34, 37, 41, 16, 26, 49, 51, 54, 30, 32, 34, 37, 41,
+      16, 26, 49, 51, 54,
+    ]
+
     const numerosSorteadosTemp = []
     const quantidadeNumerosSortear = 6
 
@@ -27,17 +30,20 @@ const MegaSenaSorteio = () => {
       }
     }
 
-    // Sortear os números
+        // Sortear os números
     for (let i = 0; i < quantidadeNumerosSortear; i++) {
       obterNumeroNaoRepetido()
     }
+
+    // Ordenar os números em ordem crescente
+    numerosSorteadosTemp.sort((a, b) => a - b);
 
     setNumerosSorteados([...numerosSorteadosTemp])
   }
 
   return (
     <div className="game">
-      <div className='logo'>
+      <div className="logo">
         <img src={megaLogo} className="logo" alt="Mega Sena logo" />
       </div>
       <h2>Números da Mega-Sena</h2>
